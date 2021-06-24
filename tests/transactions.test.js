6ac39c6fc0fcc8801886e6c184a81e2c6a46c35f
Luiz Fernando <lfaires@gmail.com>
2021-06-24 17:09:49 -0300

    installing jest and creating app and server files

---
+++ tests/transactions.test.js
@@ -0,0 +1,23 @@
+//Tenho 6 rotas
+//POST /
+//POST /sign-up
+//GET /transactions
+//POST /add-transaction/:type
+//POST /sign-out
+//DELETE /transaction/:id
+
+describe('GET /transactions', () => {
+    it('return status 200', () => {
+
+    })
+    //aqui dentro irá o teste(tantos testes quantas coisas quero testar)
+})
+
+describe('POST /transaction/:type', () => {
+    //aqui dentro irá o teste(tantos testes quantas coisas quero testar)
+})
+
+
+describe('POST /transaction/:id', () => {
+    //aqui dentro irá o teste(tantos testes quantas coisas quero testar)
+})

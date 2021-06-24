27b7510fd8c2e6d52bad69a74e8a63713fe51965
Luiz Fernando <lfaires@gmail.com>
2021-06-24 13:32:01 -0300

    delete transaction route

--- src/server.js
+++ src/server.js
@@ -89,13 +89,14 @@ app.get('/transactions', async (req,res) => {
             WHERE sessions.token = $1`,[token])
 
         const { userId, name } = user.rows[0]
+
         if(name) {
             const result = await connection.query(`
                 SELECT transactions.*, users.name as username FROM transactions 
                 JOIN users
                 ON transactions."userId" = users.id
                 WHERE "userId" = $1`,[userId])
-            if(!result.rows){
+            if(result.rows.length === 0){
                 res.send({name})
             } else {
                 res.send(result.rows)
@@ -153,6 +154,21 @@ app.post('/sign-out', async (req,res) => {
     WHERE token = $1`,[token])
 
     res.sendStatus(200)
+})
+
+app.delete('/transaction/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        await connection.query(`
+        DELETE FROM transactions
+        WHERE id = $1`,[id])
+
+        res.sendStatus(200)
+    } catch(error){
+        console.log(error)
+        res.sendStatus(501)
+    }
 })
 
 app.listen(4000, () => {
